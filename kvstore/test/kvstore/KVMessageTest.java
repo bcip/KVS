aac08c79b4fd93e8979ed3d0c65b3c781deabcae
xieyh11 <xieyh11@gmail.com>
2014-05-27 09:57:29 +0800

    Test KVMessage

--- kvstore/test/kvstore/KVMessageTest.java
+++ kvstore/test/kvstore/KVMessageTest.java
@@ -37,6 +37,64 @@ public class KVMessageTest {
         assertNull(kvm.getKey());
         assertNull(kvm.getValue());
     }
+    
+    @Test
+    public void successfullyParsesGetReq() throws KVException {
+    	setupSocket("getreq.txt");
+        KVMessage kvm = new KVMessage(sock);
+        assertNotNull(kvm);
+        assertEquals(GET_REQ, kvm.getMsgType());
+        assertNull(kvm.getMessage());
+        assertNotNull(kvm.getKey());
+        assertNull(kvm.getValue());
+    }
+    
+    @Test
+    public void successfullyParsesGetResp() throws KVException {
+        setupSocket("getresp.txt");
+        KVMessage kvm = new KVMessage(sock);
+        assertNotNull(kvm);
+        assertEquals(RESP, kvm.getMsgType());
+        assertNull(kvm.getMessage());
+        assertNotNull(kvm.getKey());
+        assertNotNull(kvm.getValue());
+    }
+    
+    @Test
+    public void unsuccessfullyParsesPutReq() throws KVException {
+        setupSocket("invalidputreq.txt");
+        try{
+        	KVMessage kvm = new KVMessage(sock);
+        	assertNotNull(kvm);
+            assertEquals(PUT_REQ, kvm.getMsgType());
+            assertNull(kvm.getMessage());
+            assertNotNull(kvm.getKey());
+            assertNotNull(kvm.getValue());
+        }catch(KVException e){
+        	assertEquals(RESP, e.getKVMessage().getMsgType());
+        	assertEquals(ERROR_INVALID_FORMAT, e.getKVMessage().getMessage());
+        }
+    }
+    
+    @Test
+    public void successfullyToXml() throws KVException {
+    	setupSocket("putreq.txt");
+    	KVMessage kvm = new KVMessage(sock);
+    	assertNotNull(kvm);
+    	String kvmToXml = kvm.toXML();
+    	Socket stringSock = mock(Socket.class);
+    	try {
+            doNothing().when(stringSock).setSoTimeout(anyInt());
+            when(stringSock.getInputStream()).thenReturn(new StringBufferInputStream(kvmToXml));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    	KVMessage kvm1 = new KVMessage(stringSock);
+    	assertEquals(kvm.getMsgType(), kvm1.getMsgType());
+    	assertEquals(kvm.getMessage(), kvm1.getMessage());
+    	assertEquals(kvm.getKey(), kvm1.getKey());
+    	assertEquals(kvm.getValue(), kvm1.getValue());
+    }
 
     /* Begin helper methods */
 
